a8d5067399fc2d6c3b68423ebab2194e62ba1423
Mirza Hilmi <118604764+MirzaHilmi@users.noreply.github.com>
2023-03-21 15:44:37 +0700

    Learn basic primitive python data types

---
+++ Variables.py
@@ -0,0 +1,17 @@
+im_string = "hello"
+
+# print(type(im_string))
+
+age = 21
+age += 2
+
+print("\nMy age is", age)
+
+floating_number = 35.879
+print("\nIm a floating number " + str(floating_number) + '\n')
+
+print("Im a number %d\nIm a string %s\nIm a decimal %f" % (age, im_string, floating_number))
+
+is_human = True
+
+print("Is it a human ? " + str(is_human))
